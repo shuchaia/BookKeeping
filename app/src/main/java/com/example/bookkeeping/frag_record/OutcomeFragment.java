ed47e1914116497514d861a2583c42ed9c73de7a
shuchaia <291775595@qq.com>
2023-08-03 16:23:30 +0800

    add记账页面布局

---
+++ app/src/main/java/com/example/bookkeeping/frag_record/OutcomeFragment.java
@@ -0,0 +1,23 @@
+package com.example.bookkeeping.frag_record;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.bookkeeping.R;
+
+public class OutcomeFragment extends Fragment {
+    public OutcomeFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_outcome, container, false);
+    }
+}
